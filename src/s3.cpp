f2f81436a7822ea16dba159f9c1221d383127d49
brewtech <evan@brewtech.us>
2014-07-17 18:16:35 -0400

    Made Awesome

--- src/s3.cpp
+++ src/s3.cpp
@@ -1,28 +1,43 @@
-#include <stdint.h>
-#include <sys/select.h>
-#include <ctype.h>
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
 #include <Rcpp.h>
-#include <libs3.h>
+#include "../inst/include/libs3.h"
 using namespace Rcpp;
+
+// [[Rcpp::export]]
+void S3Connect(const char* access_key, const char* secret_key) {
+    accessKeyIdG = access_key;
+    secretAccessKeyG = secret_key;
+}
+    
 
 
 // [[Rcpp::export]]
 void S3_init(const char* host) {
     S3Status status;
-    S3_initialize("s3",S3_INIT_ALL, host);
 
     if((status = S3_initialize("s3",S3_INIT_ALL, host)) != S3StatusOK) {
         fprintf(stderr, "Failed to initialize libs3: %s\n",
-                "404");
+                "1");
         exit(-1);
     }
 }
+
+//// [[Rcpp::export]]
+//void test_bucket(const char* host, int argc, int optindex, char *argv[]) {
+//    if(optindex == argc) {
+//        fprintf(stderr, "\nERROR: Missing parameter: bucket\n");
+//        usageExit(stderr);
+//    }
+//
+//    const char *bucketName = argv[optindex++];
+//
+//    if (optindex != argc) {
+//        fprintf(stderr, "\nERROR: Extraneous parameter: %s\n", argv[optindex]);
+//        usageExit(stderr);
+//    }
+//
+//    S3_init(host);
+//
+//
+//}
+
+
